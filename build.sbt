974b4e5ddb390f793966cc5371bfb0d03e87d363
MohamedSaeed99 <msaeed7@uic.edu>
2020-04-11 12:24:12 -0500

    Simple Connect to Github Api

--- build.sbt
+++ build.sbt
@@ -3,3 +3,18 @@ name := "maxwell_dausch_project"
 version := "0.1"
 
 scalaVersion := "2.13.1"
+
+// slf4j 1.7.30
+libraryDependencies ++= Seq("org.slf4j" % "slf4j-api" % "1.7.5",
+                            "org.slf4j" % "slf4j-simple" % "1.7.5")
+
+// logback 1.2.3
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+
+//Typesafe 1.0.2
+libraryDependencies += "com.typesafe" % "config" % "1.0.2"
+
+//HttpBuilder
+libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.5.12"
+
+
--- src/main/scala/Authenticate.scala
+++ src/main/scala/Authenticate.scala
@@ -1,3 +1,34 @@
-class Authenticate {
+import javax.xml.crypto.Data
+import org.slf4j.{Logger, LoggerFactory};
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import scala.io.Source.fromInputStream;
 
+class Authenticate{
+  val Logger = LoggerFactory.getLogger( classOf[Authenticate])
+  Logger.info("Hello from the Pizza class");
+}
+
+object main extends App{
+  val BASE_GHQL_URL = "https://api.github.com/graphql"
+  val temp="{viewer {Email Goes Here}}"
+//  implicit val formats = DefaultFormats
+
+  val client = HttpClientBuilder.create.build
+  val httpUriRequest = new HttpPost(BASE_GHQL_URL)
+  httpUriRequest.addHeader("Authorization", "Bearer GITHUB Oauth Key HERE")
+  httpUriRequest.addHeader("Accept", "application/json")
+  val gqlReq = new StringEntity("{\"query\":\"" + temp + "\"}" )
+  httpUriRequest.setEntity(gqlReq)
+
+  val response = client.execute(httpUriRequest)
+  System.out.println("Response:" + response)
+  response.getEntity match {
+    case null => System.out.println("Response entity is null")
+    case x if x != null => {
+      val respJson = fromInputStream(x.getContent).getLines.mkString
+      System.out.println(respJson)
+    }
+  }
 }
