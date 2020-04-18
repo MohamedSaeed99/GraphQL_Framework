981ec124ee5921ba5fa356ab5c0236b507428d37
Maxwell Dausch <max.dausch@gmail.com>
2020-04-17 20:09:11 -0500

    Merged

--- src/main/scala/com/cs474/ClientBuilder.scala
+++ src/main/scala/com/cs474/ClientBuilder.scala
@@ -3,19 +3,42 @@ package com.cs474
 import org.apache.http.client.methods.HttpPost
 
 // Object that is used to connect to the GitHub Api
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClientBuilder
+import scala.io.Source.fromInputStream
+
 case class GQLClient (val connectionURL:String, val headers: List[(String, String)]) {
   def display: Unit ={
     println("Current connection url: " + connectionURL)
     println("Current header list: " + headers(0)._1)
   }
 
-//  constructs a uri request with the specified headers
+  // constructs a uri request with the specified headers
   def connect: HttpPost = {
     val httpUriRequest = new HttpPost(connectionURL)
     for (header <- headers){
       httpUriRequest.addHeader(header._1, header._2)
     }
     httpUriRequest
+  }
+
+  def testConnection: Unit = {
+    val client = HttpClientBuilder.create.build
+    val connection = connect
+
+    val temp="{viewer {email login url}}"
+    val gqlReq = new StringEntity("{\"query\":\"" + temp + "\"}" )
+    connection.setEntity(gqlReq)
+
+    val response = client.execute(connection)
+    System.out.println("Response:" + response)
+    response.getEntity match {
+      case null => System.out.println("Response entity is null")
+      case x if x != null => {
+        val respJson = fromInputStream(x.getContent).getLines.mkString
+        System.out.println(respJson)
+      }
+    }
   }
 }
 
@@ -26,22 +49,22 @@ case class ClientBuilder[ConnectionParameters <: ClientBuilder.ConnectionParamet
 {
   import ClientBuilder.ConnectionParameters._
 
-//  copies and returns this object with connection Url modified
+  // copies and returns this object with connection Url modified
   def setConnectionURL(url: String): ClientBuilder[ConnectionParameters with ConnectionURL] =
     this.copy(connectionURL = url)
 
-//  copies and returns this object with the headers stored in the data structure
+  // copies and returns this object with the headers stored in the data structure
   def setHeader(key: String, value: String): ClientBuilder[ConnectionParameters with Empty] = {
     var newHeaders = headers
     newHeaders = newHeaders:+(key, value)
     this.copy(headers = newHeaders)
   }
 
-//  copies and returns this object with the headers stored in the data structure
+  // copies and returns this object with the headers stored in the data structure
   def setAuthorization(authType: String, value: String) =
     this.setHeader("Authorization", authType + " " + value)
 
-//  builds a github object
+  // builds a github object
   def build(implicit ev: ConnectionParameters =:= RequiredClientParameters): GQLClient = GQLClient(connectionURL, headers)
 }
 
--- src/main/scala/com/cs474/Main.scala
+++ src/main/scala/com/cs474/Main.scala
@@ -6,17 +6,17 @@ import org.apache.http.impl.client.HttpClientBuilder
 object Main extends App{
   val temp="{viewer {email login url}}"
 
-  val client = HttpClientBuilder.create.build
-  val client2 = new ClientBuilder[ClientBuilder.ConnectionParameters.Empty]()
+  val githubClient = new ClientBuilder[ClientBuilder.ConnectionParameters.Empty]()
     .setConnectionURL("https://api.github.com/graphql")
     .setHeader("Accept", "application/json")
     .setAuthorization("Bearer","f6f8623d4f23b15c9b60b328e9f77d49f28274a7")
     .build
 
+  val client = HttpClientBuilder.create.build
   val gqlReq = new StringEntity("{\"query\":\"" + temp + "\"}" )
-
-  val response = client.execute(client2.connect)
+  val response = client.execute(githubClient.connect)
   System.out.println("Response:" + response)
 
 
+  githubClient.testConnection
 }
